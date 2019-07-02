c5b345e1df16dffbb4ff5bd88e94955bf7f00c0c
kchikech <hkchikec@student.1337.ma>
2019-07-03 00:33:09 +0100

    add libft & GNL func

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hkchikec <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 02:13:22 by hkchikec          #+#    #+#             */
+/*   Updated: 2019/06/28 23:50:00 by hkchikec         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# define BUFF_SIZE 44
+
+int		get_next_line(const int fd, char **line);
+#endif
